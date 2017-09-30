6a52b2fd1566f980c23b47ce35b54cc4a81e1c2e
foysaliuice <foysal.iuice@gmail.com>
2017-09-30 22:36:00 +0600

    Add admin route with middleware

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//Custome middleware
+Route::get('admin','HomeController@admin')->middleware('admin');
