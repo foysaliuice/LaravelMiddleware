3be7f08259b3136b19eda51e8e1ec23bfaa9c645
foysaliuice <foysal.iuice@gmail.com>
2017-09-30 22:35:43 +0600

    Add code for showing error message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,18 @@
 
 @section('content')
 <div class="container">
+  @if (\Session::has('error'))
+    <div class="alert alert-danger">
+      {{\Session::get('error')}}
+    </div>
+  @endif
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <a href="{{url('admin')}}">Admin</a>
                 </div>
             </div>
         </div>
